fddd24cf9c9ee252f8fe31056d4759ba8bb440b7
Ryan Keys <r.keys1998@gmail.com>
2020-08-03 23:19:00 -0700

    init

--- app.py
+++ app.py
@@ -6,24 +6,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-
 def spotify_authentication():
     scope = "user-library-read"
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,client_id=env.CLIENT_ID,client_secret=env.CLIENT_SECRET,cache_path=os.getcwd()))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=env.CLIENT_ID,
+                                                   client_secret=env.CLIENT_SECRET, cache_path=os.getcwd(), redirect_uri='http://localhost:8888/callback'))
     playlists = sp.current_user_playlists()['items']
 
     for playlist in playlists:
-        print(playlist) 
+        print(playlist)
     return playlists
+
 
 @app.route('/spotify-auth')
 def spotify_auth_view():
     context = spotify_authentication()
 
-    return render_template('spotify-test.html',context=context)
+    return render_template('spotify-test.html', context=context)
