3d3ed954ab5ea4c4fd2b64682e3941fbe2bb9bad
Ryan Keys <r.keys1998@gmail.com>
2020-08-03 23:57:12 -0700

    worked with jinja handling of passed in params

--- app.py
+++ app.py
@@ -2,30 +2,21 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import env
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=env.CLIENT_ID,
+                                               client_secret=env.CLIENT_SECRET, cache_path=os.getcwd(), redirect_uri='http://localhost:8888/callback'))
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-
-
-def spotify_authentication():
-    scope = "user-library-read"
-
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=env.CLIENT_ID,
-                                                   client_secret=env.CLIENT_SECRET, cache_path=os.getcwd(), redirect_uri='http://localhost:8888/callback'))
-    playlists = sp.current_user_playlists()['items']
-
-    for playlist in playlists:
-        print(playlist)
-    return playlists
+    return render_template('index.html')
 
 
 @app.route('/spotify-auth')
 def spotify_auth_view():
-    context = spotify_authentication()
-
-    return render_template('spotify-test.html', context=context)
+    playlists = sp.current_user_playlists()['items']
+    return render_template('spotify-test.html', playlists=playlists)
