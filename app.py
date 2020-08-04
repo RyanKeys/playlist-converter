f8b500afc79d935a3b04b16f35c31a106551b69e
Ryan Keys <r.keys1998@gmail.com>
2020-08-04 00:52:39 -0700

    added playlist detail view

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-scope = "user-library-read"
+scope = "playlist-read-private"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=env.CLIENT_ID,
                                                client_secret=env.CLIENT_SECRET, cache_path=os.getcwd(), redirect_uri='http://localhost:8888/callback'))
@@ -20,3 +20,9 @@ def hello_world():
 def spotify_auth_view():
     playlists = sp.current_user_playlists()['items']
     return render_template('spotify-test.html', playlists=playlists)
+
+
+@app.route('/playlist/<playlist_id>')
+def playlist_detail_view(playlist_id):
+    playlist = sp.playlist_tracks(playlist_id)['items']
+    return render_template('playlist-detail.html', playlist=playlist)
