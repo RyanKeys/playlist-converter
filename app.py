7d80ba5a44ac0eb4d5e8eadf934612b422c0a42a
Ryan Keys <r.keys1998@gmail.com>
2020-08-05 13:24:46 -0700

    songs added nut not all

--- app.py
+++ app.py
@@ -54,13 +54,19 @@ def create_playlist_view():
         for playlist in playlists:
             if playlist['name'] in gplaylist:
                 songs = gmusic.get_playlist_contents(playlist['id'])
+        while True:
+            try:
                 for song in songs:
                     song_name = str(song['track']['title']).replace(" ", "%")
                     spotify_id = sp.search(q=song_name, type="track", limit=1)[
-                        'tracks']['items'][0]['id']
+                        'tracks']['items'][0]['uri']
                     print(spotify_id)
                     tracks.append(spotify_id)
-                    sp.user_playlist_add_tracks(
-                        user=user, playlist_id=playlist['id'], tracks=spotify_id)
+            except IndexError:
+                pass
+
+            finally:
+                sp.user_playlist_add_tracks(
+                    user=user, playlist_id=new_playlist['id'], tracks=tracks)
         return redirect(location=new_playlist['owner']['external_urls']['spotify'], code=200)
     return render_template('home/create-playlist.html', playlists=playlists, count=count, available_playlists=available_playlists)
